121497005f1029beda3e21bda4753b60792b4f4d
doplerer <carlosmarinperez1@gmail.com>
2026-04-17 10:09:21 +0200

    Añade FrontEnd hasta el punto de listado simple

---
+++ client-angular17+/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CoreModule } from './core/core.module';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CoreModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class App {
+  title = 'Tutorial de Angular';
+}
